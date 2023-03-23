ee3c627b0be64057280a17cc95a571cfc693d140
Andreyhuey <andreyhuey777@gmail.com>
2023-03-23 23:38:42 +0100

    tescom added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,7 @@ const Search = () => {
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://raw.githubusercontent.com/Andreyhuey/my-app/master/src/data/Source.json`,
+      `https://raw.githubusercontent.com/Andreyhuey/my-app/master/src/data/Superb.json`,
       {
         headers: {
           Accept: "application/vnd.github.v3+json",
@@ -17,7 +17,7 @@ const Search = () => {
     )
       .then((response) => response.json())
       .then((json) => {
-        setBeneficiaries(json.people);
+        // setBeneficiaries(json.people);
 
         console.log(json.people);
         const Results = json.people;
@@ -38,16 +38,17 @@ const Search = () => {
         className="display-5 text-warning d-flex align-items-center justify-content-center text-center"
         style={{ height: "80vh" }}
       >
-        ...Search Marvel's Database
+        ...
       </h1>
     );
 
   return (
-    <>
+    <div className="container-fluid text-white bg-dark pt-3">
       <div
-        className="container-fluid text-white bg-dark mt-5 pt-3"
+        className="container-fluid text-white bg-dark pt-3"
         style={{ height: "auto" }}
       >
+        <div className="text-center h1">beneficiaries</div>
         <div className="d-flex justify-content-center text-white my-3 py-2">
           <input
             placeholder="Goverment ID"
@@ -56,19 +57,30 @@ const Search = () => {
           />
         </div>
         <div className="">
-          <div className="text-center">beneficiaries</div>
           <div className="row">
             {beneficiaries.map((b) => {
               return (
-                <div className="col-lg-4 col-md-6 col-xs-6" key={b.Column1}>
-                  <div className="border border-warning card my-3 bg-dark">
-                    <div className="p-2 my-3">
-                      <h4 className="card-header text-center text-warning py-3">
+                <div className="col-lg-3 col-md-6 col-xs-6" key={b.Column1}>
+                  <div className="border border-white card my-3 bg-dark">
+                    <h4 className="text-center text-primary">
+                      {b["Goverment ID"]}
+                    </h4>
+                    <div className="p-3">
+                      <h6 className="card-header text-center py-3">
                         {b.Surname} {b.Firstname}
-                      </h4>
-                      <div className="">
-                        <p></p>
+                      </h6>
+
+                      <div className="pt-5 border-white">
                         <p className="d-flex justify-content-between">
+                          Provider: <b>{b.Provider}</b>
+                        </p>
+                        <p className="d-flex justify-content-between">
+                          Phone Number: <b>{b["Phone Number"]}</b>
+                        </p>
+                        <p className="d-flex justify-content-between">
+                          Policy Number: <b>{b["Policy Number"]}</b>
+                        </p>
+                        {/* <p className="d-flex justify-content-between">
                           Age: <b>{b.Age}</b>
                         </p>
                         <p className="d-flex justify-content-between">
@@ -79,19 +91,14 @@ const Search = () => {
                         </p>
                         <p className="d-flex justify-content-between">
                           LGA: <b>{b.LGA}</b>
-                        </p>
-                        <p className="d-flex justify-content-between">
-                          Government ID: <b>{b["Goverment ID"]}</b>
-                        </p>
-                        <p className="d-flex justify-content-between">
-                          Policy Number: <b>{b["Policy Number"]}</b>
-                        </p>
-                        <div>
+                        </p> */}
+
+                        <div className="align-items-center d-flex">
                           <a
                             href={b.link}
                             target="_blink"
                             rel="noreferrer"
-                            className="btn btn-outline-success"
+                            className="btn btn-lg btn-light btn-block"
                           >
                             Edit
                           </a>
@@ -105,7 +112,7 @@ const Search = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
